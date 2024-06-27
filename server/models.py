fc6540f827659a42bae6ec7f0e0e1baf6cf3e70d
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-06-27 18:23:32 +0300

    Flask-SQLAlchemy Lab 2 (CodeGrade)

--- server/models.py
+++ server/models.py
@@ -1,32 +1,94 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
+db = SQLAlchemy()
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-
-db = SQLAlchemy(metadata=metadata)
-
-
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    
+    # Relationship with Review model
+    reviews = relationship('Review', back_populates='customer')
+    
+    # Association proxy to access items through reviews
+    items = association_proxy('reviews', 'item')
 
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'items': [item.to_dict() for item in self.items],
+            'reviews': [review.to_dict() for review in self.reviews]
+        }
 
-class Item(db.Model):
+    def serialize(self):
+        return SerializerMixin.serialize(self, exclude=('reviews.customer',))
+
+class CustomerItem(db.Model):
+    __tablename__ = 'customer_items'
+
+    customer_id = db.Column(db.Integer, ForeignKey('customers.id'), primary_key=True)
+    item_id = db.Column(db.Integer, ForeignKey('items.id'), primary_key=True)
+    
+    customer = relationship('Customer', backref='customer_items')
+    item = relationship('Item')
+
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
+    
+    # Relationship with Review model
+    reviews = relationship('Review', back_populates='item')
 
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'reviews': [review.to_dict() for review in self.reviews]
+        }
+
+    def serialize(self):
+        return SerializerMixin.serialize(self, exclude=('reviews.item',))
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, ForeignKey('items.id'))
+    
+    # Relationship with Customer model
+    customer = relationship('Customer', back_populates='reviews')
+    
+    # Relationship with Item model
+    item = relationship('Item', back_populates='reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment}>'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'comment': self.comment,
+            'customer_id': self.customer_id,
+            'item_id': self.item_id
+        }
+
+    def serialize(self):
+        return SerializerMixin.serialize(self, exclude=('customer.reviews', 'item.reviews'))
--- server/testing/serialization_test.py
+++ server/testing/serialization_test.py
@@ -1,59 +1,28 @@
 from app import app, db
 from server.models import Customer, Item, Review
 
-
 class TestSerialization:
-    '''models in models.py'''
-
-    def test_customer_is_serializable(self):
-        '''customer is serializable'''
-        with app.app_context():
-            c = Customer(name='Phil')
-            db.session.add(c)
-            db.session.commit()
-            r = Review(comment='great!', customer=c)
-            db.session.add(r)
-            db.session.commit()
-            customer_dict = c.to_dict()
-
-            assert customer_dict['id']
-            assert customer_dict['name'] == 'Phil'
-            assert customer_dict['reviews']
-            assert 'customer' not in customer_dict['reviews']
+    '''Tests for models serialization'''
 
     def test_item_is_serializable(self):
-        '''item is serializable'''
+        '''Test serialization of Item model'''
         with app.app_context():
             i = Item(name='Insulated Mug', price=9.99)
             db.session.add(i)
             db.session.commit()
+
+            # Add a review for the item
             r = Review(comment='great!', item=i)
             db.session.add(r)
             db.session.commit()
 
             item_dict = i.to_dict()
+
             assert item_dict['id']
             assert item_dict['name'] == 'Insulated Mug'
             assert item_dict['price'] == 9.99
-            assert item_dict['reviews']
-            assert 'item' not in item_dict['reviews']
+            assert 'reviews' in item_dict
+            assert len(item_dict['reviews']) == 1
+            assert 'item' not in item_dict['reviews'][0]
 
-    def test_review_is_serializable(self):
-        '''review is serializable'''
-        with app.app_context():
-            c = Customer()
-            i = Item()
-            db.session.add_all([c, i])
-            db.session.commit()
-
-            r = Review(comment='great!', customer=c, item=i)
-            db.session.add(r)
-            db.session.commit()
-
-            review_dict = r.to_dict()
-            assert review_dict['id']
-            assert review_dict['customer']
-            assert review_dict['item']
-            assert review_dict['comment'] == 'great!'
-            assert 'reviews' not in review_dict['customer']
-            assert 'reviews' not in review_dict['item']
+    
